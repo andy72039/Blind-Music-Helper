e17b8ad30a6535c491a535a9ff29bf8faf969807
Peng-An <andy72039@gmail.com>
2020-03-05 00:48:12 +0800

    First commit.

---
+++ musicHelper.py
@@ -0,0 +1,42 @@
+import csv
+import music21 as m
+import gtts as g
+from pydub import AudioSegment
+
+musicTerms = []
+
+def parseMusicTerms():
+    with open('music_terms.csv', newline='', encoding='utf8') as csvfile, open('temp.txt', 'w', encoding='utf8') as f:
+        rows = csv.DictReader(csvfile)
+    
+        for row in rows:
+            musicTerms.append(row)
+        f.write(musicTerms[100]['ID'] + musicTerms[100]['Chinese'])
+    # print('finish')
+
+def readMusicXML(song):
+    c = m.converter.parse(song)
+    c.write('midi', song + '.mid')
+    c.write('musicxml', song + '.musicxml')
+
+def tts(words, lan):
+    tts = g.gTTS('Andantino', lang='it')
+    # tts.save('test.mp3')
+
+def mixSound():
+    sound1 = AudioSegment.from_mp3("input.mp3")
+    sound2 = AudioSegment.from_mp3("test.mp3")
+
+    # mix sound2 with sound1, starting at 5000ms into sound1)
+    output = sound1.overlay(sound2, position=500)
+
+    # save the result
+    output.export("mixed_sounds.mp3", format="mp3")
+
+if __name__=='__main__':
+    # song = 'reve.musicxml'
+    song = 'Una_furtiva_lagrima.mxl'
+    readMusicXML(song)
+
+    # parseMusicTerms()
+    
---
+++ run.bat
@@ -0,0 +1,3 @@
+rem set PYTHONIOENCODING=UTF-8
+"C:\Python37\python.exe" musicHelper.py 1>output.txt 2>error.txt
+output.txt
